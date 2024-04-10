b1cae82bcfa4dc8a2c07666bdb29ed2d80d6ff17
Northa <Northa@users.noreply.github.com>
2024-04-10 11:23:20 +0000

    update cons. add hashes

--- consensus.py
+++ consensus.py
@@ -1,72 +1,61 @@
-#  Github https://github.com/Northa
-
+# https://github.com/Northa/consensus/
 from urllib import request
-import math
 from sys import exit
 from json import loads
+import math
+from collections import defaultdict
+import os
 
-ERR_MSG = f"\033[91m[ERR] API endpoint unreachable: api\n" \
+
+ERR_MSG = f"\033[91m[ERR] API endpoint unreachable: _err_api_\n" \
           f"[ERR] Be sure you have enabled your API " \
           f"(you can enable this in your app.toml config file)\n" \
-          f"Bugreports Discord: Yep++#9963\033[0m"
+          f"Bugreports github: https://github.com/Northa/consensus/\033[0m"
 
 # default ports
-REST = "http://127.0.0.1:1317"
 RPC = "http://127.0.0.1:26657"
+REST = "http://127.0.0.1:1317"
 
 def handle_request(api: str, pattern: str):
     try:
-
         response = loads(request.urlopen(f"{api}/{pattern}").read())
         return response if response is not None else exit(ERR_MSG.replace('api', api))
 
-    except Exception:
-        exit(ERR_MSG.replace('api', api))
+    except Exception as err:
+        ERR_MSG.replace('_err_api_', api)
+        err = f"url: {api}/{pattern} err: {err}"
+        exit(f"{ERR_MSG}\n{err}")
+
+
+# def clear_cls():
+#     os.system('cls' if os.name=='nt' else 'clear')
 
 
 def get_validator_votes():
-
     validator_votes = []
     votes = STATE['result']['round_state']['votes']
     height = STATE['result']['round_state']['height']
     step = STATE['result']['round_state']['step']
+    cur_round = STATE['result']['round_state']['round']
     chain = get_chain_id()
+    proposer = STATE['result']['round_state']['validators']['proposer']['pub_key']['value']
     for r_ound in votes:
-        if float(r_ound['precommits_bit_array'].split('=')[-1].strip()) <= 0.66 and float(r_ound['precommits_bit_array'].split('=')[-1].strip()) > 0:
-            print(F"\nChain-id: {chain}\n"
-                  f"Height: {height} Round: {r_ound['round']} "
-                  f"step: {step}\n"
-                  f"precommits_bit_array: {r_ound['precommits_bit_array'].split('} ')[-1]}")
+        if int(r_ound['round']) != cur_round:
+            continue
 
-            for precommit in r_ound['precommits']:
-                try:
-                    validator_votes.append(precommit.split('@')[0].split(':')[1].split(' ')[0])
-                except IndexError:
-                    validator_votes.append(precommit)
-
-        elif float(r_ound['prevotes_bit_array'].split('=')[-1].strip()) <= 0.66 and float(r_ound['prevotes_bit_array'].split('=')[-1].strip()) > 0:
+        if float(r_ound['prevotes_bit_array'].split('=')[-1].strip()) > 0:
             print(F"\nChain-id: {chain}\nHeight: {height} Round: {r_ound['round']} step: {step}\nprevotes_bit_array: {r_ound['prevotes_bit_array'].split('} ')[-1]}")
-
-            for precommit in r_ound['prevotes']:
+            for prevote in r_ound['prevotes']:
                 try:
-                    validator_votes.append(precommit.split('@')[0].split(':')[1].split(' ')[0])
+                    app_hash = prevote.split("Prevote) ")[1].split(" ")[0][:3]
+                    # app_hash = f"{hashlib.sha256(app_hash.encode('utf-8')).hexdigest()[:3]}"
+                    val_vote = prevote.split('@')[0].split(':')[1].split(' ')[0]
+                    validator_votes.append([val_vote, app_hash])
                 except IndexError:
-                    validator_votes.append(precommit)
-
-    if len(validator_votes) == 0:
-        commit_votes = STATE['result']['round_state']['last_commit']
-        height = STATE['result']['round_state']['height']
-
-        if float(commit_votes['votes_bit_array'].split('=')[-1].strip()) > 0.66 and float(commit_votes['votes_bit_array'].split('=')[-1].strip()) > 0:
-            print(F"\nChain-id: {chain}\nHeight: {height} Round: {r_ound['round']} step: {step}\nvotes_bit_array: {commit_votes['votes_bit_array'].split('} ')[-1]}")
-        for commit_vote in commit_votes['votes']:
-
-            try:
-                validator_votes.append(commit_vote.split('@')[0].split(':')[1].split(' ')[0])
-            except IndexError:
-                validator_votes.append(commit_vote)
-
-    return validator_votes
+                    validator_votes.append(prevote)
+    if len(validator_votes) > 0:
+        return validator_votes, proposer
+    exit(f"height: {height} round: {cur_round} No votes found. Try in a few seconds")
 
 
 def get_validators():
@@ -84,20 +73,17 @@ def get_bonded():
 
 
 def strip_emoji_non_ascii(moniker):
-    # moniker = emoji.replace_emoji(moniker, replace='')
     moniker = "".join([letter for letter in moniker if letter.isascii()])
     return moniker[:15].strip().lstrip()
 
 
-def get_validators_rest():
+def get_validators_rest(proposer=None):
     validator_dict = dict()
     bonded_tokens = int(get_bonded()["bonded_tokens"])
     validators = handle_request(REST, '/cosmos/staking/v1beta1/validators?status=BOND_STATUS_BONDED&pagination.limit=2000')
-    # print(type(validators))
-    # for i in validators:print(i)
 
     for validator in validators['validators']:
-        # print(validator)
+
         validator_vp = int(validator["tokens"])
         vp_percentage = round((100 / bonded_tokens) * validator_vp, 3)
         moniker = validator["description"]["moniker"][:15].strip()
@@ -107,18 +93,20 @@ def get_validators_rest():
                                  "address": validator["operator_address"],
                                  "status": validator["status"],
                                  "voting_power": validator_vp,
-                                 "voting_power_perc": f"{vp_percentage}%"}
+                                 "voting_power_perc": f"{vp_percentage}%",
+                                 "voting_power_perc1": vp_percentage}
+        if proposer in validator["consensus_pubkey"]["key"]:
+            proposer = moniker
+
+    return validator_dict, proposer
 
 
-    return validator_dict, len(validators['validators'])
-
-
-def merge_info():
-    print('Bugreports discord: Yep++#9963')
-    votes = get_validator_votes()
+def merge():
+    votes, proposer = get_validator_votes()
     validators = get_validators()
     votes_and_vals = list(zip(votes, validators))
-    validator_rest, total_validators = get_validators_rest()
+    # print(votes_and_vals)
+    validator_rest, proposer = get_validators_rest(proposer)
 
     final_list = []
 
@@ -127,7 +115,7 @@ def merge_info():
             validator_rest[v[2]]['voted'] = k
             final_list.append(validator_rest[v[2]])
 
-    return final_list, total_validators
+    return final_list, proposer
 
 
 def list_columns(obj, cols=3, columnwise=True, gap=8):
@@ -155,20 +143,24 @@ def get_chain_id():
     return chain_id
 
 
-
 def colorize_output(validators):
     result = []
+    validators = sorted(validators, key=lambda x: x['voting_power'], reverse=True)
     for num, val in enumerate(validators):
-        vp_perc = f"{val['voting_power_perc']:<7}"
+        vp_perc = f"{val['voting_power_perc']:<12}"
         moniker = val['moniker']
 
         if val['voted'] != 'nil-Vote':
             stat = f"\033[92m{num+1:<3} {'ONLINE':<8} \033[0m"
-            result.append(f"{stat} {moniker:<18} {vp_perc}")
+            # stat = f"{num+1:<3} {'🟢':<2}"
+            app_hash = f"{val['voted'][1]}".upper()
+
+            result.append(f"{stat} {moniker:<16}{app_hash} {vp_perc}")
 
         else:
             stat = f"\033[91m{num+1:<3} {'OFFLINE':<8} \033[0m"
-            result.append(f"{stat} {moniker:<18} {vp_perc}")
+            # stat = f"{num+1:<3} {'🔴':<2}"
+            result.append(f"{stat} {moniker:<16}--- {vp_perc}")
 
     return result
 
@@ -176,54 +168,69 @@ def colorize_output(validators):
 def calculate_colums(result):
     if len(result) <= 30:
         return list_columns(result, cols=1)
-    elif 30 < len(result) <= 100:
+    elif 30 < len(result) <= 60:
         return list_columns(result, cols=2)
-    elif 100 < len(result) <= 150:
+    elif 60 < len(result) <= 150:
         return list_columns(result, cols=3)
     else:
         return list_columns(result, cols=4)
 
 
-def get_pubkey_by_valcons(valcons, height):
-    response = handle_request(REST, f"/validatorsets/{height}")
-    for validator in response['result']['validators']:
-        if valcons in validator['address']:
-            return validator['pub_key']['value']
+# def get_pubkey_by_valcons(valcons, height):
+#     response = handle_request(REST, f"/validatorsets/{height}")
+#     for validator in response['result']['validators']:
+#         if valcons in validator['address']:
+#             return validator['pub_key']['value']
 
 
-def get_moniker_by_pub_key(pub_key, height):
-    response = handle_request(REST, f"cosmos/staking/v1beta1/historical_info/{height}")
-    for validator in response['hist']['valset']:
+# def get_moniker_by_pub_key(pub_key, height):
+#     response = handle_request(REST, f"cosmos/staking/v1beta1/historical_info/{height}")
+#     for validator in response['hist']['valset']:
 
-        if pub_key in validator['consensus_pubkey']['key']:
-            return strip_emoji_non_ascii(validator['description']['moniker'])
+#         if pub_key in validator['consensus_pubkey']['key']:
+#             return strip_emoji_non_ascii(validator['description']['moniker'])
 
 
-def get_evidence(height):
-    evidences = handle_request(REST, '/cosmos/evidence/v1beta1/evidence')
-    for evidence in evidences['evidence']:
-        if int(height) - int(evidence['height']) < 1000:
-            pub_key = get_pubkey_by_valcons(evidence['consensus_address'], evidence['height']).strip()
-            moniker = get_moniker_by_pub_key(pub_key, evidence['height'])
-            # print(colored(f"Evidence: {moniker}\nHeight: {evidence['height']} {evidence['consensus_address']} power: {evidence['power']}\n", 'yellow'))
-            print(f"\033[93mEvidence: {moniker}\nHeight: {evidence['height']} {evidence['consensus_address']} power: {evidence['power']}\033[0m\n")
+# def get_evidence(height):
+#     evidences = handle_request(REST, '/cosmos/evidence/v1beta1/evidence')
+#     for evidence in evidences['evidence']:
+#         if int(height) - int(evidence['height']) < 1000:
+#             pub_key = get_pubkey_by_valcons(evidence['consensus_address'], evidence['height']).strip()
+#             moniker = get_moniker_by_pub_key(pub_key, evidence['height'])
+#             # print(colored(f"Evidence: {moniker}\nHeight: {evidence['height']} {evidence['consensus_address']} power: {evidence['power']}\n", 'yellow'))
+#             print(f"\033[93mEvidence: {moniker}\nHeight: {evidence['height']} {evidence['consensus_address']} power: {evidence['power']}\033[0m\n")
 
 
 def main(STATE):
-    validators, total_validators = merge_info()
-
+    validators, proposer = merge()
     online_vals = 0
+    votes = defaultdict(float)
     for num, val in enumerate(validators):
-        if val['voted'] != 'nil-Vote':
+        if val['voted'] == 'nil-Vote':
+            votes['nil-Vote'] += val['voting_power_perc1']
+        else:
             online_vals += 1
+            votes[val['voted'][1]] += val['voting_power_perc1']
+    print(f"Proposer: {proposer}")
+    print('\nConsensus:')
+    sum_vp = 0
+    for k, v in votes.items():
+        if not 'nil' in k:
+            sum_vp += round(v,2)
+            print(f"{f'hash {k}'}: {round(v,2):.2f}%")
+        else:
+            print(f"{k}: {round(v,2):.2f}%")
 
-    print(f"Online: {online_vals}/{total_validators}\n")
-    # get_evidence(STATE['result']['round_state']['height'])
+    print(f"\n{'Online_vp':<17}: {sum_vp:.2f}%")
+    print(f"Online validators: {online_vals}/{len(validators)}\n")
+    # get_evidence(STATE['result']['round_state']['height/round/step'])
     result = colorize_output(validators)
     print(calculate_colums(result))
 
 
 if __name__ == '__main__':
-    STATE = handle_request(RPC, 'dump_consensus_state')
-
-    exit(main(STATE))
+    try:
+        STATE = handle_request(RPC, 'dump_consensus_state')
+        exit(main(STATE))
+    except KeyboardInterrupt:
+        exit(1)
